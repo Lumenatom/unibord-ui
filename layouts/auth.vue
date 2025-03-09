843fb4b9efb727601349f9664a38f13c5e0a9388
Arthur Shevtsov <lumenatom@gmail.com>
2025-03-09 13:58:41 +0200

    setup

---
+++ layouts/auth.vue
@@ -0,0 +1,32 @@
+<!-- layouts/auth.vue -->
+
+<template>
+  <div class="auth-layout">
+    <div class="auth-container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// В этом лейауте мы можем также подключать компоненты, необходимые только для аутентификации
+</script>
+
+<style scoped>
+.auth-layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #f5f5f5;
+}
+
+.auth-container {
+  background: white;
+  padding: 2rem;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  max-width: 400px;
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!--    <NuxtRouteAnnouncer />-->
+    <!--    <NuxtWelcome />-->
+    <div class="bg-blue-500 text-white p-4 rounded">
+      Hello, Tailwind CSS is working!
+    </div>
+    <Button>Click me</Button>
+  </div>
+  <div>
+    <p>Текущая тема: {{ colorMode.preference }}</p>
+    <button @click="toggleTheme">Switch Theme</button>
+
+  </div>
+  <div class="bg-prim text-white">
+    Привет мир!
+  </div>
+  <Button
+      variant="outline" @click="() => {
+      toast('Event has been created', {
+        description: 'Sunday, December 03, 2023 at 9:00 AM',
+        action: {
+          label: 'Undo',
+          onClick: () => console.log('Undo'),
+        },
+      })
+    }"
+  >
+    Add to calendar
+  </Button>
+  <BeakerIcon class="size-6 text-blue-500" />
+
+</template>
+<script setup>
+import { toast } from 'vue-sonner'
+import { BeakerIcon } from '@heroicons/vue/24/solid'
+
+const colorMode = useColorMode()
+
+const toggleTheme = () => {
+  console.log("Current mode:", colorMode.preference)
+  colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light'
+}
+onMounted(async () => {
+  const config = useRuntimeConfig()
+  const apiUrl = config.public.API_URL
+
+  try {
+    const response = await useFetch(`${apiUrl}/test`)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+})
+
+</script>
